1a5ef28ef9d60635f95d9701fe6070bee45fdb13
inkahirsila <inka.hirsila@gmail.com>
2022-10-10 15:06:42 +0300

    valmis

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import  React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className ='footer'>
+            <div className='container'>
+                <span>Please sticky footer here</span>
+
+            </div>
+
+        </footer>
+    )
+}
